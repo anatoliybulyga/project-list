59fc8475071480220aba35f59ab71fa8b15032b7
anatoliybulyga <103635183+anatoliybulyga@users.noreply.github.com>
2025-01-09 16:22:31 +0200

    Handle click favorite projects

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,20 +1,26 @@
 import React from "react";
 import { Menu } from "antd";
 import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
 const Sidebar = () => {
   const projects = useSelector((state) => state.projects);
   const favoriteProjects = projects.filter((project) => project.isFavorite);
+  const navigate = useNavigate();
+
+  const handleClick = (e) => {
+    navigate(`/details/${e.key}`);
+  };
 
   const items = [
-    { key: "favorite-projects", label: "Favorite Projects" },
+    { key: "favorite-projects", label: "Favorite Projects", type: "group" },
     ...favoriteProjects.map((project) => ({
       key: project.id,
       label: project.name
     }))
   ];
 
-  return <Menu className="mt-8" theme="dark" mode="inline" items={items} />;
+  return <Menu className="mt-8" theme="dark" mode="inline" items={items} onClick={handleClick} />;
 };
 
 export default Sidebar;
--- src/pages/ProjectDetails.jsx
+++ src/pages/ProjectDetails.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
-// import { fetchProjectById } from '../path/to/your/actions'; // Adjust the import based on your project structure
 
 const ProjectDetails = () => {
   const { id } = useParams();
@@ -9,12 +8,6 @@ const ProjectDetails = () => {
   const dispatch = useDispatch();
   const project = useSelector((state) => state.projects.find((project) => project.id === id));
   const [projectData, setProjectData] = useState(project);
-
-  //   useEffect(() => {
-  //     if (!project) {
-  //       dispatch(fetchProjectById(id));
-  //     }
-  //   }, [id, project, dispatch]);
 
   useEffect(() => {
     if (project) {
@@ -36,36 +29,41 @@ const ProjectDetails = () => {
 
   return (
     <div className="max-w-2xl mx-auto p-6 bg-white shadow-md rounded-lg mt-10 text-lg">
-      <div className="mb-4 flex justify-between">
-        <strong className="font-medium text-right w-1/3 mr-6">Project ID</strong>
-        <span className="w-2/3 text-left">{projectData.id}</span>
+      <div className="mb-4 flex flex-col sm:flex-row justify-between">
+        <strong className="font-medium text-right sm:w-1/3 mr-6">Project ID</strong>
+        <span className="sm:w-2/3 text-left">{projectData.id}</span>
       </div>
-      <div className="mb-4 flex justify-between">
-        <strong className="font-medium text-right w-1/3 mr-6">Project Name</strong>
-        <span className="w-2/3 text-left">{projectData.name}</span>
+      <div className="mb-4 flex flex-col sm:flex-row justify-between">
+        <strong className="font-medium text-right sm:w-1/3 mr-6">Project Name</strong>
+        <span className="sm:w-2/3 text-left">{projectData.name}</span>
       </div>
-      <div className="mb-4 flex justify-between">
-        <strong className="font-medium text-right w-1/3 mr-6">Description</strong>
-        <span className="w-2/3 text-left">{projectData.description}</span>
+      <div className="mb-4 flex flex-col sm:flex-row justify-between">
+        <strong className="font-medium text-right sm:w-1/3 mr-6">Description</strong>
+        <span className="sm:w-2/3 text-left">{projectData.description}</span>
       </div>
-      <div className="mb-4 flex justify-between">
-        <strong className="font-medium text-right w-1/3 mr-6">Start Date</strong>
-        <span className="w-2/3 text-left">{projectData.startDate}</span>
+      <div className="mb-4 flex flex-col sm:flex-row justify-between">
+        <strong className="font-medium text-right sm:w-1/3 mr-6">Start Date</strong>
+        <span className="sm:w-2/3 text-left">{projectData.startDate}</span>
       </div>
-      <div className="mb-4 flex justify-between">
-        <strong className="font-medium text-right w-1/3 mr-6">End Date</strong>
-        <span className="w-2/3 text-left">{projectData.endDate}</span>
+      <div className="mb-4 flex flex-col sm:flex-row justify-between">
+        <strong className="font-medium text-right sm:w-1/3 mr-6">End Date</strong>
+        <span className="sm:w-2/3 text-left">{projectData.endDate}</span>
       </div>
-      <div className="mb-10 flex justify-between">
-        <strong className="font-medium text-right w-1/3 mr-6">Project Manager</strong>
-        <span className="w-2/3 text-left">{projectData.manager}</span>
+      <div className="mb-10 flex flex-col sm:flex-row justify-between">
+        <strong className="font-medium text-right sm:w-1/3 mr-6">Project Manager</strong>
+        <span className="sm:w-2/3 text-left">{projectData.manager}</span>
       </div>
-      <button onClick={handleBack} className="mr-2 ml-10 px-10 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">
-        Back
-      </button>
-      <button onClick={handleEdit} className="px-10 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">
-        Edit
-      </button>
+      <div className="flex flex-col sm:flex-row sm:justify-start ml-12">
+        <button
+          onClick={handleBack}
+          className="mb-2 sm:mb-0 sm:mr-2 px-8 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
+        >
+          Back
+        </button>
+        <button onClick={handleEdit} className="px-8 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">
+          Edit
+        </button>
+      </div>
     </div>
   );
 };
